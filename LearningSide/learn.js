3af7e3c16bc15a28664153847386a52944ab302f
Abhinxv <114543771+damnordinarycodes@users.noreply.github.com>
2026-02-04 02:09:37 +0530

    Update learn.js

---
+++ LearningSide/learn.js
@@ -0,0 +1,17 @@
+//string methods = allow youy to manipuklater and work with text (strings)
+
+//let userName = "BroCode";
+//userName = userName.trim(); //to trim whitespaces
+//userName = userName.toUpperCase(); //to make everything uppercase
+//userName = userName.toLowerCase(); // make everythibng lower case
+//userName = userName.repeat(100); // repeats it the n number of times
+//console.log(userName.lastIndexOf / indexOf / length ("r"));
+//console.log(userName.charAt(4));
+//The charAt() method in JavaScript returns the character at 
+// a specified index (position) within a string
+// let phoneNumber = "1234-231-4123";
+
+// phoneNumber = phoneNumber.padStart(1532, "342"); //adds a padding to the number and gives the 
+//                                                  // padding number to be the , wali value
+
+// console.log(phoneNumber);
