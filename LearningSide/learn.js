b2a7add0d165401701d195f9b3b4c3e69c2cd837
Abhinxv <114543771+damnordinarycodes@users.noreply.github.com>
2026-02-03 16:57:16 +0530

    Update learn.js

---
+++ LearningSide/learn.js
@@ -0,0 +1,8 @@
+// ternary operator in java script = a shortcut to if{} and else{} statements
+// helps to assign a variable based on a condition 
+// condition ? codeIfTrue : codeIfFalse;
+
+let age = 21;
+
+age>= 18 ? "You are an adult" : "you are minor";
+
