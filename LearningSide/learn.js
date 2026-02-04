0afa85ff8640c6d5cd342f3b8460b18ec663d3d6
Abhinxv <114543771+damnordinarycodes@users.noreply.github.com>
2026-02-04 15:20:38 +0530

    Update learn.js

--- LearningSide/learn.js
+++ LearningSide/learn.js
@@ -1,6 +1,27 @@
-//method chaining = calling one method after another in one continous line of code.
+// //method chaining = calling one method after another in one continous line of code.
+
+// const { useReducer } = require("react");
 
 
-//no method chaining
+// //no method chaining
 
-let username = window.prompt("enter your username");
+// let username = window.prompt("enter your username");
+
+// username - username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
+
+// let extraChar = username.slice(1);
+// extraChar = extraChar.toLowerCase();
+
+
+
+// username = letter + extraChar;
+// console.log(username);
+
+
+// // method chaining
+
+// username = username.trim().charAt(0).toUpperCase + username.trim().slice(1).toLowerCase();
+
+// console.log(username);
