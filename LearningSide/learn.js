81db4f87aa8ae2e83e0fda50422ad62fee0dd677
Abhinxv <114543771+damnordinarycodes@users.noreply.github.com>
2026-07-14 11:28:47 +0530

    Update learn.js

--- LearningSide/learn.js
+++ LearningSide/learn.js
@@ -1,1 +1,13 @@
-//rest parameters - 
+//callback a functionm that is passed as an argumment to another function
+
+
+hello();
+goodbye();
+
+function hello(){
+    console.log("Hello, World!")
+}
+
+function goodbye(){
+    console.log("Goodbye, World!")
+}
